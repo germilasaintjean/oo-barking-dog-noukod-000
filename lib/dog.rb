99767c82eb2b2d1b6a84b0164a30aea60e13a466
Saint-Jean Germila <germila@noukod.com>
2018-09-06 17:21:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,6 +4,9 @@ class Dog
     @name = name
   end
 
+   def name
+     @name
+   end
   def bark=(bark = "woof!")
     @bark
   end
@@ -11,7 +14,7 @@ end
 
    fido = Dog.new
    fido.name = "Fido"
-
+   
    fido.name
 
     fido.bark
