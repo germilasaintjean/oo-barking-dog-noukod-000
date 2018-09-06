86c4470e22b2244a6ae7adacc7d0e7e5e88d1197
Saint-Jean Germila <germila@noukod.com>
2018-09-06 17:13:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+class Dog
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark=(bark)
+    @bark = bark
+  end
+
+  def bark
+    @bark
+  end
+end
+
+   fido = Dog.new
+   fido.name = "Fido"
+   fido.name
+
+   fido.bark = "woof!"
+   fido.bark
